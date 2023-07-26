75dc964b0d1d93f73d4900403a030618ca8a6657
Dave Hurst <david@hurst.com>
2023-07-25 19:16:15 -0700

    build index file

--- siahl_ringers/make_datafiles.R
+++ siahl_ringers/make_datafiles.R
@@ -3,9 +3,11 @@
 
 
 ## App deployment -- ignore warnings
+setwd("~/Documents/GitHub/siahl-ringers-branch")
 
 library(here)
-setwd("~/Documents/GitHub/siahl-ringers-branch")
+source('siahl-utils.R')
+
 load(here("siahl-eda-Current.qmd.RData"))
 saveRDS(all_teams, file = "siahl_ringers/all_teams.RDS" )
 
@@ -21,3 +23,26 @@ affected_games <- games_adj %>%
     mutate(n_ringer_goals = h_goals + v_goals - hg_adj - hg_adj) %>% 
     arrange(-n_ringer_goals)
 write_csv(affected_games, file = "affected_games.csv")
+
+html_text <- readLines(here("app_demo/index_template.html"))
+find_line <- which(str_detect(html_text, "AWAY_TEAM vs HOME_TEAM"))
+template_line <- html_text[find_line]
+
+example_games <- character()
+for (i in 1:7) {
+    game <- affected_games[i, ]
+    item_html <- template_line %>% 
+        str_replace('HOME_TEAM', game$Home) %>% 
+        str_replace('AWAY_TEAM', game$Away) %>% 
+        str_replace('OUTCOME', wlt_outcome(game$h_goals, game$v_goals)) %>% 
+        str_replace('OUT_ADJ', wlt_outcome(game$hg_adj, game$vg_adj)) %>%
+        str_replace('NRG', game$n_ringer_goals %>% as.character())
+    example_games <- c(example_games, item_html)
+}
+
+new_text <- c(
+    html_text[1:(find_line - 1)],
+    example_games,
+    html_text[(find_line + 1):length(html_text)]
+)
+writeLines(new_text, con = file("app_demo/index.html"))
