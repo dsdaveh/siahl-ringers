d04118163267b8b28e3a80c71ac970c477a2e85d
Dave Hurst <david@hurst.com>
2023-07-31 17:17:10 -0700

    remove links to example games

--- siahl_ringers/make_datafiles.R
+++ siahl_ringers/make_datafiles.R
@@ -23,26 +23,4 @@ affected_games <- games_adj %>%
     arrange(-n_ringer_goals)
 write_csv(affected_games, file = "affected_games.csv")
 
-html_text <- readLines(here("app_demo/index_template.html"))
-find_line <- which(str_detect(html_text, "AWAY_TEAM vs HOME_TEAM"))
-template_line <- html_text[find_line]
 
-example_games <- character()
-for (i in 1:7) {
-    game <- affected_games[i, ]
-    item_html <- template_line %>% 
-        str_replace_all('GAMEID', game$Game) %>% 
-        str_replace('HOME_TEAM', game$Home) %>% 
-        str_replace('AWAY_TEAM', game$Away) %>% 
-        str_replace('OUTCOME', wlt_outcome(game$h_goals, game$v_goals)) %>% 
-        str_replace('OUT_ADJ', wlt_outcome(game$hg_adj, game$vg_adj)) %>%
-        str_replace('NRG', game$n_ringer_goals %>% as.character())
-    example_games <- c(example_games, item_html)
-}
-
-new_text <- c(
-    html_text[1:(find_line - 1)],
-    example_games,
-    html_text[(find_line + 1):length(html_text)]
-)
-writeLines(new_text, con = file("app_demo/index.html"))
