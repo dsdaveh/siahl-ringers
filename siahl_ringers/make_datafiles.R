348aa776a5c7221f10d5b868d9c65214d68b7505
Dave Hurst <david@hurst.com>
2023-08-09 13:57:32 -0700

    affect_games fix

--- siahl_ringers/make_datafiles.R
+++ siahl_ringers/make_datafiles.R
@@ -7,20 +7,22 @@
 library(here)
 source(here('siahl-utils.R'))
 
+shiny_dir <- 'siahl_ringers'
+
 load(here('data', "siahl-eda-Current.qmd.RData"))
-saveRDS(all_teams, file = here("siahl_ringers/all_teams-Current.RDS" ))
+saveRDS(all_teams, file = here(shiny_dir, "all_teams-Current.RDS" ))
 
 affected_game_ids <- games_adj %>% filter(adj_effect) %>% pull(Game)
 affected_scorecards <- scorecards
 for(gid in names(affected_scorecards)) {
     if (! gid %in% affected_game_ids) affected_scorecards[[gid]] <- NULL
 }
-saveRDS(affected_scorecards, file = here("siahl_ringers/scorecards.RDS" ))
+saveRDS(affected_scorecards, file = here(shiny_dir, "scorecards.RDS" ))
 
 affected_games <- games_adj %>% 
     filter(adj_effect) %>% 
     mutate(n_ringer_goals = h_goals + v_goals - hg_adj - hg_adj) %>% 
     arrange(-n_ringer_goals)
-write_csv(affected_games, file = "affected_games.csv")
+write_csv(affected_games, file = hear(shiny_dir, "affected_games.csv")
 
 
